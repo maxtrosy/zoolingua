b4c0aae10d1e85479f97b90c5503ed848f3991a0
maxtrosy <maxtrosy@gmail.com>
2024-06-03 22:05:06 -0500

    ?

---
+++ lib/login.dart
@@ -0,0 +1,224 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFFA2D7FE), // Color de fondo del AppBar
+        title: SizedBox.shrink(), // Elimina el texto del título
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(25.0), // Altura de la imagen
+          child: Center(
+            child: Image.asset(
+              'assets/banner.png', // Ruta de la imagen
+              height: 80, // Ajusta la altura de la imagen según sea necesario
+            ),
+          ),
+        ),
+      ),
+      body: Stack(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Flexible(
+                flex: 3,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(height: 170),
+                    // Ajustamos el tamaño de fuente para que sea más pequeño
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Container(
+                        constraints: BoxConstraints(maxWidth: MediaQuery.of(context).size.width * 0.8),
+                        child: Center(
+                          child: Text(
+                            '¡Bienvenid@!',
+                            style: TextStyle(
+                              color: Color(0xFF5DAEF1), // Color del recuadro inferior
+                              fontSize: 50,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 50.0),
+                      child: Row(
+                        children: [
+                          Text('Usuario:', textAlign: TextAlign.start),
+                        ],
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                      ),
+                    ),
+                    SizedBox(height: 9),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 40.0),
+                      child: Row(
+                        children: [
+                          Flexible(
+                            child: TextField(
+                              textAlign: TextAlign.left,
+                              decoration: InputDecoration(
+                                filled: true,
+                                fillColor: Color(0xFFF5F5F4),
+                                hintText: 'Ingrese su usuario',
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(25.0),
+                                  borderSide: BorderSide.none,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 16),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 40.0),
+                      child: Row(
+                        children: [
+                          Text('Contraseña:', textAlign: TextAlign.start),
+                        ],
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 40.0),
+                      child: Row(
+                        children: [
+                          Flexible(
+                            child: TextField(
+                              textAlign: TextAlign.left,
+                              decoration: InputDecoration(
+                                filled: true,
+                                fillColor: Color(0xFFF5F5F4),
+                                hintText: 'Ingrese su contraseña',
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(25.0),
+                                  borderSide: BorderSide.none,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 9),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                          child: TextButton(
+                            onPressed: () {
+                              // Handle forgot password button tap
+                            },
+                            child: Text(
+                              '¿Olvidaste tu contraseña?',
+                              style: TextStyle(
+                                color: Colors.blue,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Container(
+                  color: Color(0xFFA2D7FE),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.8,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            // Handle login button tap
+                          },
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all<Color>(
+                              Color(0xFF5DAEF1),
+                            ),
+                            minimumSize: MaterialStateProperty.all<Size>(
+                              Size(MediaQuery.of(context).size.width * 0.8, 50),
+                            ),
+                            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(25.0),
+                              ),
+                            ),
+                          ),
+                          child: Text('Iniciar sesión'),
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      Container(
+                        height: 1,
+                        width: MediaQuery.of(context).size.width * 0.8,
+                        color: Color(0xFF5DAEF1),
+                      ),
+                      SizedBox(height: 16),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.8,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            // Handle create account button tap
+                          },
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all<Color>(
+                              Colors.white,
+                            ),
+                            minimumSize: MaterialStateProperty.all<Size>(
+                              Size(MediaQuery.of(context).size.width * 0.8, 50),
+                            ),
+                            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(25.0),
+                              ),
+                            ),
+                          ),
+                          child: Text('Crear una cuenta'),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: Center(
+              child: Image.asset(
+                'assets/lingua.png', // Ruta de la imagen
+                width: 180, // Ajusta el tamaño de la imagen según sea necesario
+                height: 180,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Login Demo',
+    home: LoginScreen(),
+  ));
+}
